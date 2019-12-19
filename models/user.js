a3071e510f2b68739f8aaf72f5946301d3ae8a41
davidroman-hub <jobroman83@gmail.com>
2019-12-19 15:57:24 +0100

    finishing user Squema

--- models/user.js
+++ models/user.js
@@ -3,10 +3,39 @@ const crypto = require('crypto')
 const uuidv1 = require('uuid/v')
 
 const userSchema = new mongoose.Schema({
-    name:{
+    
+    name: {
         type: String,
         trim: true,
         required:true,
         maxlength:32
+    },
+
+    email: {
+        type: String,
+        trim: true,
+        required:true,
+        unique:32
+    },
+
+    hashed_password: {
+        type: String,
+        required:true
+        
+    },
+
+    about: {
+        type: String,
+        trim: true
+    },
+    salt: String,
+    role: {
+        type:Number,
+        default:0
+    },
+
+    history:{
+        type: Array,
+        default:[]
     }
-})
+},{timestamps: true})
