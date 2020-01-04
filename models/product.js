6e44c5976b86d88e17dce43ca597028d1f908596
unknown <jobroman83@gmail.com>
2020-01-04 20:56:31 +0100

    product model

---
+++ models/product.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+const {ObjectId} = mongoose.Schema
+
+const productSchema = new mongoose.Schema(
+    {
+        name:{
+            type:String,
+            trim:true,
+            required:true,
+            maxlength:32
+        },
+        description:{
+            type:String,
+            required:true,
+            maxlength:2000
+        },
+        price:{
+            type:Number,
+            trim:true,
+            required:true,
+            maxlength:32
+        },
+        category:{
+            type:ObjectId, // that why i brought the object id , when i create the category it will be here.
+            ref:'Category',// this is the name of the category name ;)
+            required:true
+        },
+    },
+    {timestamps:true}
+);
+
+module.exports = mongoose.model("Product", productSchema);
