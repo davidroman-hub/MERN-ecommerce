69e5527e8308965a2d98c64371057ee989c485e6
davidroman-hub <jobroman83@gmail.com>
2020-01-06 13:09:20 +0100

    auth from all the fields, i mean everyone need to be required

--- controllers/product.js
+++ controllers/product.js
@@ -13,11 +13,32 @@ exports.create = (req, res) => {
             if(err){
                 return res.status(400).json({
                     error:'image could not be uploaded'
-                })
+                });
             }
+
+                //check for all the fields are required
+
+                const {name, description, price, category,shipping, quantity} = fields
+
+                if(!name || !description || !price || !category || !shipping || !quantity){
+                    return res.status(400).json({
+                        error:'All fields are required'
+                    })
+                }
+
+
                 let product = new Product(fields)
 
+                //1kb == 1000
+                //1mb = 1 000 000
+
                 if(files.photo){
+                    //console.log('FILES PHOTO:', files.photo); // to see the size of the photo
+                    if(files.photo.size >1000000){ //this is for auth the photo is less than 1 mb
+                        return res.status(400).json({
+                            error:"the image should be less than 1 mb size"
+                        })
+                    }
                     product.photo.data = fs.readFileSync(files.photo.path)
                     product.photo.contentType = files.photo.type
                 }
