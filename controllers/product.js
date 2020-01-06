153ef6860f2408734334333f8ec074e43d4bc944
davidroman-hub <jobroman83@gmail.com>
2020-01-06 14:02:44 +0100

    Product Delete method

--- controllers/product.js
+++ controllers/product.js
@@ -72,7 +72,23 @@ exports.create = (req, res) => {
 
                     res.json(result)
 
-                })
-          })
+                });
+          });
 
-        }
+        };
+
+    exports.remove = (req, res) => {
+        
+    let product = req.product
+        product.remove((err, deletedProduct)=>{
+            if(err){
+                return res.status(400).json({
+                    error:errorHandler(err)
+                });
+            }
+            res.json({
+            //    deletedProduct,
+             "message":"Product deleted succesfully"
+            })
+        })
+    }    
