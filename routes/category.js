04001f4dc8b80eb10e572718e639303a5e316acf
davidroman-hub <jobroman83@gmail.com>
2020-01-07 18:17:40 +0100

    Adding update, delate and list all the categories

--- routes/category.js
+++ routes/category.js
@@ -1,20 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const { create, categoryById,read } = require('../controllers/category');
+const { create, categoryById,read,update,remove,list } = require('../controllers/category');
 const {requireSignin, isAuth, isAdmin} = require('../controllers/auth');
 const { userById } = require('../controllers/user');
 
-router.post(
-    
-    '/category/create/:userId',
-    requireSignin,
-    isAdmin,
-    isAuth,
-    create 
-    
-    );
-router.get('/category/:categoryId', read)
+router.get('/category/:categoryId', read);
+router.post('/category/create/:userId',requireSignin,isAdmin,isAuth,create);
+router.put('/category/:categoryId/:userId',requireSignin,isAdmin,isAuth,update);  
+router.delete('/category/:categoryId/:userId',requireSignin,isAdmin,isAuth,remove);  
+router.get('/categories',list);
 
 
 
