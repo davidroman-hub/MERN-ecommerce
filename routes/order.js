3600c9669bc706ee7334951288a7b7f38d798007
davidroman-hub <jobroman83@gmail.com>
2020-02-25 18:16:42 +0100

    adding history

--- routes/order.js
+++ routes/order.js
@@ -4,10 +4,18 @@ const router = express.Router();
 // need to be auth and signin thats why we used the middleware here
 const {requireSignin, isAuth} = require('../controllers/auth')
 //also we gonna use user controllers as well
-const { userById } = require('../controllers/user')
+const { userById, addOrderToUserHistory } = require('../controllers/user')
+
 const { create } = require('../controllers/order')
 
-router.post('/order/create/:userId', requireSignin, isAuth, create);
+
+router.post(
+    "/order/create/:userId",
+    addOrderToUserHistory,
+    create,
+    requireSignin,
+    isAuth,
+    )
 
 
 router.param('userId', userById)
